10b31bdf5b8e53b4f2f67865559cf735768ce40f
Ryan Lynch <ReLynch91@gmail.com>
2020-05-02 10:29:02 -0700

    "continuing with flexbox and re-structuring sign-in form"
    "adding more detail"

--- frontend/components/Session/sign_up_form.jsx
+++ frontend/components/Session/sign_up_form.jsx
@@ -31,87 +31,58 @@ class SignUpForm extends React.Component {
     render() {
         return (
             <div>
-
-                <div className="sign-up-nav-links">
-                    <i class="fa fa-angle-left" aria-hidden="true"></i>
-                    <Link to="/">
-                         Community Home
-                   </Link>
-                </div>
-
-                <div className="create-user-form">
+                <div className="sign-up-nav-links"><Link to="/">Community Home </Link></div>
+                
+                <div className="form-header">
                     <div className="sign-up-header-login">
                         <h1>Sign Up</h1>
                         <ul>
-                            <p>Existing User?</p>
-                            <Link to="/login">Sign in</Link>
+                            <p>Existing User?</p><Link to="/login">Sign in</Link>
                         </ul>
                     </div>
 
-                    <form className="user-information" onSubmit={this.handleSubmit}>
-                        <label>Display Name
-                            <input
-                                type="text"
-                                value={this.state.username}
-                                onChange={this.update('username')}
-                            />
-                        </label>
-                        <label>Email Address
-                            <input
-                                type="email"
-                                value={this.state.email}
-                                onChange={this.update('email')}
-                            />
-                        </label>
-                        <label>Password
-                            <input
-                                type="password"
-                                value={this.state.password}
-                                onChange={this.update('password')}
-                            />
-                        </label>
-                        <label>Confirm Password
-                            <input
-                                type="password"
-                                value={this.state.confirmPassword}
-                                onChange={this.update('confirmPassword')}
-                            />
-                        </label>
-                        <label>Location
-                            <input
-                                type="text"
-                                value={this.state.location}
-                                onChange={this.update('location')}
-                            />
-                        </label>
-                        <label>Interests
-                            <textarea
-                                value={this.state.interests}
-                                onChange={this.update('interests')}
-                            />
-                        </label>
-                        <label>Sailing Aspirations and History
-                            <input
-                                type="text"
-                                value={this.state.experience}
-                                onChange={this.update('experience')}
-                            />
-                        </label>
-                        <input type="checkbox" id="newsUpdates" />
-                        <label htmlFor='newsUpdates'>Send me news and updates</label><br></br>
+                    <form className="user-form" onSubmit={this.handleSubmit}>
+                        <ul>
+                            <label>Display Name
+                                <input type="text" value={this.state.username} onChange={this.update('username')} />
+                            </label>
 
-                        <input type="checkbox" />
-                        <label htmlFor='checkbox'> Sign in anonymously</label><br></br>
-                        <br />
-                        <button onSubmit={this.handleSubmit}>Create my Account</button>
+                            <label>Email Address
+                                <input type="email"value={this.state.email} onChange={this.update('email')} /> 
+            
+                            </label>
+                            <label>Password
+                                <input type="password" value={this.state.password} onChange={this.update('password')} />
+                            </label>
+
+                            <label>Confirm Password
+                                <input type="password" value={this.state.confirmPassword} onChange={this.update('confirmPassword')} />
+                            </label>
+
+                            <label>Location
+                                <input type="text" value={this.state.location} onChange={this.update('location')} />
+                            </label>
+
+                            <label>Interests
+                                <textarea value={this.state.interests} onChange={this.update('interests')} />
+                            </label>
+
+                            <label>Sailing Aspirations and History
+                                <input type="text" value={this.state.experience} onChange={this.update('experience')} />
+                            </label>
+                            <input type="checkbox" id="newsUpdates" />
+                            <label htmlFor='newsUpdates'>Send me news and updates</label><br></br>
+
+                            <input type="checkbox" />
+                            <label htmlFor='checkbox'> Sign in anonymously</label>
+
+                            <button onSubmit={this.handleSubmit}>Create my Account</button>
+                        </ul>
                     </form>
-
                     <div>
                         {this.props.errors}
                     </div>
-
                 </div>
-
             </div>
         )
     }
